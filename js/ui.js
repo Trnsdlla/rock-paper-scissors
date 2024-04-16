5c1ea57b60f7f8fc5c3914491adb61149b50b9b6
Michael Trandella <107638701+Trnsdlla@users.noreply.github.com>
2024-04-16 16:42:19 -0400

    Update to enhance UI interaction + game logic

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,40 @@
-const rockBtn = document.createElement("button")
-const paperBtn = document.createElement("button")
-const scissorsBtn = document.createElement("button")
+const rockBtn = document.getElementById("rockBtn");
+const paperBtn = document.getElementById("paperBtn");
+const scissorsBtn = document.getElementById("scissorsBtn");
 
+rockBtn.addEventListener("click", function() {
+    if (playerScore < 5 && computerScore < 5) {
+        const result = playRound("rock", getComputerChoice());
+        document.getElementById("results").textContent = `${result}\nPlayer Score: ${playerScore} ; Computer Score: ${computerScore}`;
+        checkGameEnd();
+    }
+});
+paperBtn.addEventListener("click", function() {
+    if (playerScore < 5 && computerScore < 5) {
+        const result = playRound("paper", getComputerChoice());
+        document.getElementById("results").textContent = `${result}\nPlayer Score: ${playerScore} ; Computer Score: ${computerScore}`;
+        checkGameEnd();
+    }
+});
+scissorsBtn.addEventListener("click", function () {
+    if (playerScore < 5 && computerScore < 5) {
+        const result = playRound("scissors", getComputerChoice());
+        document.getElementById("results").textContent = `${result}\nPlayer Score: ${playerScore} ; Computer Score: ${computerScore}`;
+        checkGameEnd();
+    }
+});
+
+function checkGameEnd() {
+    if (playerScore === 5 || computerScore === 5) {
+        let finalMessage = "Game Over. ";
+        finalMessage += playerScore === 5 ? "You win!" : "Computer Wins!";
+        document.getElementById("results").textContent += `\n${finalMessage}`;
+        disableButtons();
+    }
+};
+
+function disableButtons() {
+    rockBtn.disabled = true;
+    paperBtn.disabled = true;
+    scissorsBtn.disabled = true;
+}
