90f6549500581be587f9cc69130b0dd51d5fdb00
Michael Trandella <107638701+Trnsdlla@users.noreply.github.com>
2024-04-16 16:41:17 -0400

    Major updates to refine logic for UI interaction

--- js/game.js
+++ js/game.js
@@ -1,6 +1,5 @@
 let playerScore = 0;
 let computerScore = 0;
-let roundsPlayed = 0;
 
 function randomNumber() {
     return Math.floor(Math.random() * 3)
@@ -11,64 +10,27 @@ function getComputerChoice() {
     
     switch (choice) {
         case 0:
-            return "Rock";
+            return "rock";
         case 1: 
-            return "Paper";
+            return "paper";
         case 2: 
-            return "Scissors";
+            return "scissors";
         default: 
             return "Computer Error";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (!playerSelection) {
-        return "Game cancelled";
-    }
 
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-
-    if ((playerSelection === "rock" && computerSelection === "scissors") ||
+    if (playerSelection === computerSelection) {
+        return "Its a tie!";
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")) {
         playerScore++;
         return "You win!";
-    } else if ((playerSelection === "scissors" && computerSelection === "rock") ||
-               (playerSelection === "rock" && computerSelection === "paper") ||
-               (playerSelection === "paper" && computerSelection === "scissors")) {
-                computerScore++;
-                return "Computer wins!"
-    } else if (playerSelection === computerSelection) {
-        return "Its a tie!";
     } else {
-        return "Invalid Selection";
-    }
+        computerScore++;
+        return "Computer wins!";
+    };
 }
-
-function playGame() {
-
-        const playerSelection = prompt("Enter rock, paper, or scissors.");
-
-        if(playerSelection === null) {
-            console.log("Game canceled by the player.");
-            return;
-        }
-
-        if (playerSelection && (playerSelection.toLowerCase() === 'rock' || playerSelection.toLowerCase() === 'paper' || playerSelection.toLowerCase() === 'scissors')) {
-            const computerSelection = getComputerChoice();
-            const result = playRound(playerSelection, computerSelection);
-
-            alert("Player Score: " + playerScore + "\nComputer Score: " + computerScore + "\nResult: " + result);
-
-            roundsPlayed++;
-        } else {
-            alert("Invalid selection. Please enter 'rock', 'paper', or 'scissors'.");
-        }
-        console.log("Player Score:", playerScore);
-        console.log("Computer Score:", computerScore);
-
-        playGame();
-    }
-
-playGame();
